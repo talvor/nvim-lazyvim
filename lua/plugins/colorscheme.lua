33d7f08918493df7f72b6ee621f5c24003ec66e7
Phillip Hall <phall@atlassian.com>
2025-04-11 17:41:55 +1000

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavor = "frappe",
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
---
+++ lua/plugins/default.lua
@@ -0,0 +1,12 @@
+return {
+  { "folke/tokyonight.nvim", enabled = false },
+
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+}
